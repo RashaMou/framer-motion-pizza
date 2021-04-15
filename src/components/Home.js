b80aa3a76d1c5cc9f296fa1e635961876818045c
Rasha Moumneh <rasha@fastmailteam.com>
2021-04-14 22:28:09 -0400

    create loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { Link } from "react-router-dom";
+import Loader from "./Loader";
 
 const containerVariants = {
     hidden: { opacity: 0 },
@@ -38,6 +39,7 @@ const Home = () => {
                     Shoo baddak 3laya?
                 </motion.button>
             </Link>
+            <Loader />
         </motion.div>
     );
 };
